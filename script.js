2f036e4cc0c1662df591cd3e23edc4cb597f4587
Kenneth Huddleston <kenneth.j.huddleston@gmail.com>
2023-01-12 13:57:39 -0600

    reworks javascript to allow for multiple operators

--- script.js
+++ script.js
@@ -3,58 +3,64 @@ const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator');
 const equals = document.getElementById('=');
 
-const oneKey = document.getElementById(1);
-const twoKey = document.getElementById(2);
-const threeKey = document.getElementById(3);
-const fourKey = document.getElementById(4);
-const fiveKey = document.getElementById(5);
-const sixKey = document.getElementById(6);
-const sevenKey = document.getElementById(7);
-const eightKey = document.getElementById(8);
-const nineKey = document.getElementById(9);
-const zeroKey = document.getElementById(0);
-const periodKey = document.getElementById(".");
-const deleteKey = document.getElementById("delete");
-const divideKey = document.getElementById("/");
-const multiplyKey = document.getElementById("*");
-const subtractKey = document.getElementById("-");
-const addKey = document.getElementById('+');
-
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-
+let numberOne = undefined;
+let numberTwo = undefined;
+let displayed = [];
 let currentNumber = [];
-let num1 = [];
-let currentOperator = [];
+let currentOperator = undefined;
+let total = 0;
+
+const operate = (operator, numberOne, numberTwo) => {
+    if (operator === '+') {
+        return numberOne + numberTwo;
+    } else if (operator === '-') {
+        return numberOne - numberTwo;
+    } else if (operator === '*') {
+        return numberOne * numberTwo;
+    } else if (operator === '/') {
+        return numberTwo / numberTwo;
+    } else {
+        console.log('ERROR IN OPERATE FUNCTION');
+    }
+};
 
 number.forEach((currentKey) => {
     currentKey.addEventListener('click', function() {
         currentNumber.push(Number(currentKey.id));
-        display.innerText = currentNumber.join('');
+        displayed.push(Number(currentKey.id));
+        display.innerText = displayed.join('');
     })
 });
 
 operator.forEach((currentKey) => {
     currentKey.addEventListener('click', function () {
-        num1.push(Number(currentNumber.join(''))); 
-        currentNumber = [];
-        display.innerText = currentNumber.join('');
-        currentOperator.push(currentKey.id);
+        if (currentNumber[0] === undefined) {
+            return;
+        } else {
+            currentOperator = currentKey.id;
+        }
+        if (numberOne === undefined) {
+            numberOne = Number(currentNumber.join(''));
+            currentNumber = [];
+        } else if (numberTwo === undefined) {
+            numberTwo = Number(currentNumber.join(''));
+            total = operate(currentOperator, numberOne, numberTwo);
+            numberOne = total;
+            numberTwo = undefined;
+            currentNumber = [];
+        }
+        displayed.push(currentKey.id);
+        display.innerText = displayed.join('');
     })
 });
 
 equals.addEventListener('click', function () {
-    let firstNumber = Number(num1.join(''));
-    let secondNumber = Number(currentNumber.join(''));
-    if (currentOperator[0] === '+') {
-        display.innerText = firstNumber + secondNumber;
-    } else if (currentOperator[0] === '-') {
-        display.innerText = firstNumber - secondNumber;
-    } else if (currentOperator[0] === '*') {
-        display.innerText = firstNumber * secondNumber;
-    } else if (currentOperator[0] === '/') {
-        display.innerText = firstNumber / secondNumber;
-    }
-})
+    numberTwo = Number(currentNumber.join(''));
+    total = operate(currentOperator, numberOne, numberTwo);
+    numberOne = undefined;
+    numberTwo = undefined;
+    currentNumber = [];
+    displayed = [];
+    currentOperator = undefined;
+    display.innerText = total;
+});
